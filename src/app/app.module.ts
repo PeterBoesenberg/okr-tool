6ab644882667be6d1a349891c86f1fa5c38ae16b
Peter Bösenberg <peterboesenerg@gmail.com>
2019-07-26 14:44:58 +0200

    flex layout, accordion for introduction, skeleten components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,13 @@
-import { BrowserModule } from "@angular/platform-browser";
+import { LayoutModule } from "@angular/cdk/layout";
 import { NgModule } from "@angular/core";
-
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { HomeComponent } from "./home/home.component";
+import { MaterialModule } from "./material.module";
 import { NavigationComponent } from "./navigation/navigation.component";
-import { LayoutModule } from "@angular/cdk/layout";
-import { MatToolbarModule } from "@angular/material/toolbar";
-import { MatButtonModule } from "@angular/material/button";
-import { MatSidenavModule } from "@angular/material/sidenav";
-import { MatIconModule } from "@angular/material/icon";
-import { MatListModule } from "@angular/material/list";
-import { HomeComponent } from './home/home.component';
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 @NgModule({
   declarations: [AppComponent, NavigationComponent, HomeComponent],
@@ -20,11 +16,8 @@ import { HomeComponent } from './home/home.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     LayoutModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule
+    FlexLayoutModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/introduction/introduction.module.ts
+++ src/app/introduction/introduction.module.ts
@@ -1,10 +1,29 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { MatExpansionModule } from "@angular/material/expansion";
+import { MatFormFieldModule } from "@angular/material/form-field";
 import { IntroductionRoutingModule } from "./introduction-routing.module";
-import { IntroductionComponent } from './introduction/introduction.component';
+import { IntroductionComponent } from "./introduction/introduction.component";
+import { AnalyzeComponent } from "./analyze/analyze.component";
+import { PlanComponent } from "./plan/plan.component";
+import { OnboardingComponent } from "./onboarding/onboarding.component";
+import { MatChipsModule } from "@angular/material/chips";
 
 @NgModule({
-  declarations: [IntroductionComponent],
-  imports: [CommonModule, IntroductionRoutingModule]
+  declarations: [
+    IntroductionComponent,
+    AnalyzeComponent,
+    PlanComponent,
+    OnboardingComponent
+  ],
+  imports: [
+    CommonModule,
+    IntroductionRoutingModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatChipsModule
+  ]
 })
-export class IntroductionModule {}
+export class IntroductionModule {
+  panelOpenState: boolean;
+}
